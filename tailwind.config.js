c57f12648b5886219c79b4810306382ffa13bee3
iamgois <thiagogoisjunior@gmail.com>
2023-06-23 16:58:19 -0300

    end

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontSize: {
+        xss: '0.625rem'
+      }
     },
   },
   plugins: [],
